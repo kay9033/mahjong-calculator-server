084f98e77cb2a2e31df504e41f08c9caef53f75e
kay9033 <kay903347@gmail.com>
2025-02-02 20:05:32 +0900

    環境構築

---
+++ src/main/java/com/youkawa/mahjong_calculator/MahjongCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.youkawa.mahjong_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MahjongCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MahjongCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/youkawa/mahjong_calculator/MahjongCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youkawa.mahjong_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MahjongCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
