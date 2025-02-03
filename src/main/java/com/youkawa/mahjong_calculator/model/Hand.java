e4f212e743d79b84df0bc8008ff3ee71d87313ad
kay9033 <kay903347@gmail.com>
2025-02-03 20:41:31 +0900

    Handクラスの作成

---
+++ src/main/java/com/youkawa/mahjong_calculator/model/Hand.java
@@ -0,0 +1,16 @@
+package com.youkawa.mahjong_calculator.model;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Hand {
+  private List<Tile> tiles;
+  private boolean idDealer;
+  private boolean isRiichi;
+  private boolean isTsumo;
+  private List<String> spesialConditions;
+}
