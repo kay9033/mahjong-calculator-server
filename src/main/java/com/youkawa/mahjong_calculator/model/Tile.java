c7afedb6b26619f643dfb500ea84d08eaf88efd8
kay9033 <kay903347@gmail.com>
2025-02-10 22:10:51 +0900

    Tileクラスの作成

---
+++ src/main/java/com/youkawa/mahjong_calculator/model/Tile.java
@@ -0,0 +1,11 @@
+package com.youkawa.mahjong_calculator.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Tile {
+  private String suit;
+  private int number;
+}
