a16e65765b9c8710064554184ee220929cf8a156
kay9033 <kay903347@gmail.com>
2025-02-10 21:58:09 +0900

    お題の追加

--- src/main/java/com/youkawa/mahjong_calculator/service/QuestionService.java
+++ src/main/java/com/youkawa/mahjong_calculator/service/QuestionService.java
@@ -11,7 +11,12 @@ import com.youkawa.mahjong_calculator.model.Question;
 @Service
 public class QuestionService {
   private final List<Question> questionList = List.of(
-      new Question(1, List.of("混一色", "チャンタ"), Map.of("場風", "東", "自風", "南"), "ron"));
+      new Question(1, List.of("混一色", "チャンタ"),
+          Map.of("場風", "東", "自風", "南"), "ron"),
+      new Question(2, List.of("平和", "一盃口"),
+          Map.of("場風", "南", "自風", "西"), "tsumo"),
+      new Question(3, List.of("対々和", "役牌"),
+          Map.of("場風", "西", "自風", "北"), "ron"));
 
   public Question getRandomQuestion() {
     Random random = new Random();
