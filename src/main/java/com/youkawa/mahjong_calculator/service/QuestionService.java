44d85f0e6804028809fd3d751f8308817f6d598d
kay9033 <kay903347@gmail.com>
2025-02-25 20:51:27 +0900

    出題用手牌の作成

--- src/main/java/com/youkawa/mahjong_calculator/service/QuestionService.java
+++ src/main/java/com/youkawa/mahjong_calculator/service/QuestionService.java
@@ -1,32 +1,96 @@
 package com.youkawa.mahjong_calculator.service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
 import org.springframework.stereotype.Service;
 
-import com.youkawa.mahjong_calculator.model.Question;
+import com.youkawa.mahjong_calculator.model.Tile;
 
 @Service
 public class QuestionService {
-  private final List<Question> questionList = List.of(
-      new Question(1, List.of("混一色", "チャンタ"),
-          Map.of("場風", "東", "自風", "南"), "ron"),
-      new Question(2, List.of("平和", "一盃口"),
-          Map.of("場風", "南", "自風", "西"), "tsumo"),
-      new Question(3, List.of("対々和", "役牌"),
-          Map.of("場風", "西", "自風", "北"), "ron"));
+  private static final Random random = new Random();
+  private static final List<String> SUITS = Arrays.asList("m", "s", "p", "z");
+  private static final Map<String, Integer> MAX_NUMBERS = Map.of("m", 9, "s", 9, "p", 9, "z", 7);
+  private static final List<String> NAKI_TYPE = Arrays.asList(null, "ron", "ti");
 
-  public Question getRandomQuestion() {
-    Random random = new Random();
-    return questionList.get(random.nextInt(questionList.size()));
+  private List<List<Tile>> genarateHand() {
+    List<List<Tile>> hand = new ArrayList<>();
+
+    if (random.nextBoolean()) {
+      for (int i = 0; i < 6; i++) {
+        hand.add(generateToitsu());
+      }
+    } else {
+      for (int i = 0; i < 4; i++) {
+        if (random.nextBoolean()) {
+          hand.add(generateKotsu());
+        } else {
+          hand.add(generateShuntsu());
+        }
+      }
+    }
+    hand.add(generateToitsu());
+
+    return hand;
   }
 
-  public Question getQuestionById(int id) {
-    return questionList.stream()
-        .filter(question -> question.getId() == id)
-        .findFirst()
-        .orElse(null);
+  private List<Tile> generateKotsu() {
+    String suit = SUITS.get(random.nextInt(SUITS.size() - 1));
+    int number;
+    List<Tile> meld = new ArrayList<>();
+
+    if (suit == "z") {
+      number = random.nextInt(7) + 1;
+    } else {
+      number = random.nextInt(9) + 1;
+    }
+    for (int i = 0; i < 3; i++) {
+      Tile tile;
+      if (i == 2) {
+        tile = new Tile(suit, number, NAKI_TYPE.get(random.nextInt(SUITS.size() - 1)));
+      } else {
+        tile = new Tile(suit, number, null);
+        meld.add(tile);
+      }
+    }
+    return meld;
   }
+
+  private List<Tile> generateShuntsu() {
+    String suit = SUITS.get(random.nextInt(SUITS.size() - 2));
+    int number = random.nextInt(3) * 3 + 1;
+    List<Tile> meld = new ArrayList<>();
+    for (int i = 0; i < 3; i++) {
+      Tile tile;
+      if (i == 2) {
+        tile = new Tile(suit, number + i, NAKI_TYPE.get(random.nextInt(SUITS.size() - 1)));
+      } else {
+        tile = new Tile(suit, number + i, null);
+        meld.add(tile);
+      }
+    }
+    return meld;
+  }
+
+  private List<Tile> generateToitsu() {
+    String suit = SUITS.get(random.nextInt(SUITS.size() - 1));
+    int number;
+    List<Tile> meld = new ArrayList<>();
+
+    if (suit == "z") {
+      number = random.nextInt(7) + 1;
+    } else {
+      number = random.nextInt(9) + 1;
+    }
+    for (int i = 0; i < 2; i++) {
+      Tile tile = new Tile(suit, number, null);
+      meld.add(tile);
+    }
+    return meld;
+  }
+
 }
